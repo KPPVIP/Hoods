1bbe2bbbf6244d352973c43772b48e8080ed087b
Guudz <Kilian1702@outlook.com>
2023-02-02 07:18:49 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,25 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/quartier2.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/baklava.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/binalar.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/deadly_hood.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/deadly_kellypark.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/grove.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/iskender.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/taco.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/basket.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/wld_district_hat.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/wld_district_house_1.ytyp'
